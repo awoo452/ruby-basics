625772072e7ef80da4a308cac828990112d279d4
Aaron <awoo452@wgu.edu>
2023-10-12 19:22:47 -0700

    added basic_ruby_projects folder and subfolders

---
+++ The Odin Project/basic_ruby_projects/caesar_cipher/caesar_cipher.rb
@@ -0,0 +1,10 @@
+#Implement a caesar cipher that takes in a string and the shift factor and then outputs the modified string
+#It is a type of substitution cipher in which each letter in the plaintext is replaced by a letter some fixed number of positions down the alphabet. 
+#For example, with a left shift of 3, D would be replaced by A, E would become B, and so on.
+
+#> caesar_cipher("What a string!", 5)
+#=> "Bmfy f xywnsl!"
+
+def caesar_cipher(string, shift_factor)
+    
+end
