33ac9ab70e4e2c8b6e5250ca9ce49581dbccef67
Aaron <awoo452@wgu.edu>
2023-10-22 19:26:30 -0700

    Caesar Cipher Project Completed

--- The Odin Project/basic_ruby_projects/caesar_cipher/caesar_cipher.rb
+++ The Odin Project/basic_ruby_projects/caesar_cipher/caesar_cipher.rb
@@ -11,19 +11,28 @@
     # irb -r ./caesar_cipher.rb will open this file in terminal
 
 def caesar_cipher(string, shift_factor)
+        def is_upper?
+          self == self.upcase
+        end
+      
+        def is_lower?
+          self == self.downcase
+        end
         # *** https://stackoverflow.com/questions/4846853/generate-array-of-all-letters-and-digits 
         # *** https://apidock.com/ruby/Kernel/Array/instance
-    alphabet = 'a'.upto('z').to_a #This establishes a variable containing the alphabet, separated by letter as an Array
+    alphabet = 'a'.upto('z').to_a #This establishes an Array containing the alphabet, separated by letter
+    alphabetButBigger = 'A'.upto('Z').to_a
+    alphabet.push(*alphabetButBigger)
         # *** https://stackoverflow.com/questions/6085518/what-is-the-easiest-way-to-push-an-element-to-the-beginning-of-the-array
-    alphabet.unshift(" ", "-") #This adds compatibility for strings containing spaces, dashes, etc. This can be revisited to further expand compatibility for provided characters but the logic is there. 
+    alphabet.unshift(" ") #This adds compatibility for strings containing spaces. This can be revisited to further expand compatibility for provided characters but the logic is there. 
     stringConvertedToArray = string.split("") #This takes the user provided "string", and converts it into an array separated by each individual character in the user provided string.
         # *** https://www.theodinproject.com/lessons/ruby-basic-enumerable-methods - Scroll to Map section
         # *** https://apidock.com/ruby/Array/index
     arrayConvertedFromStringsToCharacters = stringConvertedToArray.map { |letter| alphabet.index(letter) } #for each letter in stringConvertedToArray, find the corresponding letter in the alphabet array and replace with the index of that letter within the alphabet.
-    shiftFactoredArrayOfNumbers = arrayConvertedFromStringsToCharacters.map { |number| number + shift_factor } #add the user provided 'shift_factor' to each index value of this array.
+    shiftFactoredArrayOfNumbers = arrayConvertedFromStringsToCharacters.map { |number| number + shift_factor } #add the user provided 'shift_factor' to each corresponding number within this array.
         # *** https://stackoverflow.com/questions/26415380/replace-a-single-element-in-an-array
-    shiftFactoredArrayOfNumbersWithoutOvers = shiftFactoredArrayOfNumbers.map { |number| number >= alphabet.count() ? number -= alphabet.count() : number } #If the number is > the total number of characters in the alphabet, restart at the beginning of the alphabet array
-    shiftFactoredArrayOfNumbersWithoutOversOrUnders = shiftFactoredArrayOfNumbersWithoutOvers.map { |number| number < 0 ? number += alphabet.count() : number } #If the number is < 0, restart at the end of the alphabet array
-    shiftFactoredArrayOfCharacters = shiftFactoredArrayOfNumbersWithoutOversOrUnders.map { |number| alphabet[number] } #This compares the shifted number to the corresponding value contained at that index in the alphabet array and creates a new array containing these 'shifted' characters
-    return shiftFactoredArrayOfCharacters
+    shiftFactoredArrayOfNumbersWithoutOvers = shiftFactoredArrayOfNumbers.map { |number| number >= alphabet.count() ? number -= alphabet.count() : number } #If after shift factoring the number, the total is > the total number of characters in the alphabet array, restart at the beginning of the alphabet array
+    shiftFactoredArrayOfNumbersWithoutOversOrUnders = shiftFactoredArrayOfNumbersWithoutOvers.map { |number| number < 0 ? number += alphabet.count() : number } #If after shift factoring the number, the total is < 0, reset the count by adding the total number of characters in the alphabet array so that it loops from z-a 
+    shiftFactoredArrayOfCharacters = shiftFactoredArrayOfNumbersWithoutOversOrUnders.map { |number| alphabet[number] } #This compares the shifted number to the corresponding value contained at that index in the alphabet array and creates a new array containing these 'shifted' characters, thus completing the cipher
+    print shiftFactoredArrayOfCharacters
 end
