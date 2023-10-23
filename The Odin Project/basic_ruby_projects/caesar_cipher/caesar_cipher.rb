100d9456df1d86a3de29ce87f1ccbfe15f097bb7
Aaron <awoo452@wgu.edu>
2023-10-22 18:32:24 -0700

    Caesar Cipher Project Completed

--- The Odin Project/basic_ruby_projects/caesar_cipher/caesar_cipher.rb
+++ The Odin Project/basic_ruby_projects/caesar_cipher/caesar_cipher.rb
@@ -13,14 +13,15 @@ def caesar_cipher(string, shift_factor)
         # *** https://apidock.com/ruby/Kernel/Array/instance
     alphabet = 'a'.upto('z').to_a #This establishes a variable containing the alphabet, separated by letter as an Array
         # *** https://stackoverflow.com/questions/6085518/what-is-the-easiest-way-to-push-an-element-to-the-beginning-of-the-array
-    alphabet.unshift(" ", "-") #This adds compatibility for strings containing spaces, dashes, etc. This can be revisited to further expand compatibility for provided characters but the logic is there.
-    convertStringToArray = string.split("") #This takes the user provided "string", and converts it into an array separated by each individual character in the user provided string.
+    #alphabet.unshift(" ", "-") #This adds compatibility for strings containing spaces, dashes, etc. This can be revisited to further expand compatibility for provided characters but the logic is there. 
+    stringConvertedToArray = string.split("") #This takes the user provided "string", and converts it into an array separated by each individual character in the user provided string.
         # *** https://www.theodinproject.com/lessons/ruby-basic-enumerable-methods - Scroll to Map section
         # *** https://apidock.com/ruby/Array/index
-    convertArrayFromStringsToNumbers = convertStringToArray.map { |letter| alphabet.index(letter) } #for each letter in stringToArray, find the corresponding letter in the alphabet array and replace with the index of that letter within the alphabet.
-    addShiftFactorToArrayOfNumbers = convertArrayFromStringsToNumbers.map { |number| number + shift_factor } #add the user provided 'shift_factor' to each index value of this array.
-    convertArrayOfShiftFactoredNumbers = addShiftFactorToArrayOfNumbers.map { |number| alphabet[number] } #This compares the shifted number to the corresponding value contained at that index in the alphabet array and creates a new array containing these 'shifted' characters
-        #this works, but I need to figure out how to loop back to the beginning or end of the array should the shift_factor push past 'Z', (or 'A' if the shift_factor is a negative number)
-    
-    return convertArrayOfShiftFactoredNumbers
+    arrayConvertedFromStringsToCharacters = stringConvertedToArray.map { |letter| alphabet.index(letter) } #for each letter in stringToArray, find the corresponding letter in the alphabet array and replace with the index of that letter within the alphabet.
+    shiftFactoredArrayOfNumbers = arrayConvertedFromStringsToCharacters.map { |number| number + shift_factor } #add the user provided 'shift_factor' to each index value of this array.
+        # *** https://stackoverflow.com/questions/26415380/replace-a-single-element-in-an-array
+    shiftFactoredArrayOfNumbersWithoutOvers = shiftFactoredArrayOfNumbers.map { |number| number >= alphabet.count() ? number -= alphabet.count() : number } #If the number is > the total number of characters in the alphabet, restart at the beginning of the alphabet array
+    shiftFactoredArrayOfNumbersWithoutOversOrUnders = shiftFactoredArrayOfNumbersWithoutOvers.map { |number| number < 0 ? number += alphabet.count() : number } #If the number is < 0, restart at the end of the alphabet array
+    shiftFactoredArrayOfCharacters = shiftFactoredArrayOfNumbersWithoutOversOrUnders.map { |number| alphabet[number] } #This compares the shifted number to the corresponding value contained at that index in the alphabet array and creates a new array containing these 'shifted' characters
+    return shiftFactoredArrayOfCharacters
 end
