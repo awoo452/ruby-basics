eb1316959d308079b7faad192e0c5c969e0f57d1
Aaron <awoo452@wgu.edu>
2023-10-24 22:21:17 -0700

    Sub Strings Project Completed

--- The Odin Project/basic_ruby_projects/sub_strings/sub_strings.rb
+++ The Odin Project/basic_ruby_projects/sub_strings/sub_strings.rb
@@ -1,6 +1,15 @@
 #Implement a method #substrings that takes a word as the first argument and then an array of valid substrings (your dictionary) as the second argument. 
 #It should return a hash listing each substring (case insensitive) that was found in the original string and how many times it was found.
-
+def substrings(words, dictionary)
+    downcasedWords = words.downcase #bitch you're lowercase now
+    arrayOfDowncasedWords = downcasedWords.split(" ") #this takes the user provided 'words' and converts it into an array of words, downcased, and separated by the user provided spaces.
+     # *** https://www.theodinproject.com/lessons/ruby-basic-enumerable-methods - scroll to reduce section
+    results = arrayOfDowncasedWords.reduce(Hash.new(0)) do |instances, word| 
+        instances[word] += 1
+        instances
+    end
+    results
+end
 #> dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 #=> ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 #> substrings("below", dictionary)
